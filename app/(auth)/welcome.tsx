7383ba60903783e0ff692d1954fa3a8a2a327963
hollali <dheztinykartel@gmail.com>
2025-07-20 20:48:43 +0000

    `Rename Welcome component to Onboarding`

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Welcome = () => {
+const Onboarding = () => {
 	return (
 		<View>
 			<Text>Welcome</Text>
@@ -9,4 +9,4 @@ const Welcome = () => {
 	);
 };
 
-export default Welcome;
+export default Onboarding;
