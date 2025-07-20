59f5e02910a6054dfe456e0eee3ed3a32a50d3b7
hollali <dheztinykartel@gmail.com>
2025-07-20 22:31:25 +0000

    `feat: updated welcome.tsx with custom button and image display`

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,13 +1,15 @@
+import CustomButton from "@/components/customButton";
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import React, { useRef } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from 'react-native-swiper';
 
 const Onboarding = () => {
     const swiperRef = useRef<Swiper>(null);
     const [activeIndex, setActiveIndex] = React.useState(0);
+    const isLastSlide = activeIndex === onboarding.length - 1;
 	return (
 		<SafeAreaView className="h-full items-center justify-between bg-white">
             <TouchableOpacity
@@ -23,11 +25,18 @@ const Onboarding = () => {
             onIndexChanged={(index) => setActiveIndex(index)}
             >
                 {onboarding.map((item) => (
-                    <View>
-                        <Text>{item.title}</Text>
+                    <View key={item.id} className="flex items-center justify-center p-5">
+                        <Image source={item.image} className="w-full h-[300px]" resizeMode="contain" />
+                        <View className="flex flex-row items-center justify-center w-full mt-10">
+                            <Text className="text-black text-3xl font-bold mx-10 text-center">{item.title}</Text>
+                        </View>
+                        <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">{item.description}</Text>
                     </View>
                 ))}
             </Swiper>
+            <CustomButton title={isLastSlide ? "Get Started" : "Next"}
+            onPress={() => isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current?.scrollTo(activeIndex + 1)} 
+            className="w-11/12 mt-10"/>
         </SafeAreaView>
     );
 };
