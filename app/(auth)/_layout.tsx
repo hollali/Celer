f9e7514a78c44b2c045cf677916dd90e3a875cfa
hollali <dheztinykartel@gmail.com>
2025-07-20 20:52:11 +0000

    `Added new screen "welcome" to auth layout and redirected Home component to new screen`

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -10,6 +10,8 @@ const Layout = () => {
 			<Stack.Screen name="Welcome" options={{ headerShown: false }} />
             <Stack.Screen name="sign-up" options={{ headerShown: false }} />
             <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+            <Stack.Screen name="welcome" options={{ headerShown: false }} />
+
 		</Stack>
 	);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,9 @@
+import { Redirect } from 'expo-router'
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 
 const Home = () => {
-  return (
-    <SafeAreaView className='flex-1 items-center justify-center'>
-      <Text className='text-danger-700 font-JakartaExtraBold'>Home</Text>
-    </SafeAreaView>
-  )
+  return <Redirect href="/(auth)/welcome" />
 }
 
 export default Home
