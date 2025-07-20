81032f87fb1926490648d15f6a6fb3239b805384
hollali <dheztinykartel@gmail.com>
2025-07-20 21:14:41 +0000

    `Added SafeAreaView and TouchableOpacity to Onboarding component, replaced existing View and Text components, and added router navigation to skip button.`

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
 import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Onboarding = () => {
 	return (
-		<View>
-			<Text>Welcome</Text>
-		</View>
+		<SafeAreaView className="h-full items-center justify-between bg-white">
+            <TouchableOpacity
+            onPress={() => {router.replace("/(auth)/sign-up")}}  
+            >
+                <Text>Skip</Text>
+            </TouchableOpacity>
+		</SafeAreaView>
 	);
 };
 
