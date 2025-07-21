796dc74ec413247bb430c0e384e2f5e1c71f693f
hollali <dheztinykartel@gmail.com>
2025-07-21 09:55:31 +0000

    `Added image and layout components to sign-up page`

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import InputField from "@/components/inputField";
+import { images } from "@/constants";
 import React from "react";
+import { Image, ScrollView, View, Text } from "react-native";
 
 const SignUp = () => {
 	return (
-		<View>
-			<Text> SignUp</Text>
-		</View>
+		<ScrollView className="flex-1 bg-white">
+			<View className="flex-1 bg-white">
+				<View className="relative w-full h-[250px]">
+					<Image
+					source={images.signUpCar}
+					className="z-0 w-full h-[250px]"
+					/>
+					<Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+						Create Your Account
+					</Text>
+				</View>
+				<View className="p-5">
+					{/*<InputField/>*/}
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
 
