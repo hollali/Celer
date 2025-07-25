19e283fa5cbef7e889f57ecf7eac84c560d552c4
hollali <dheztinykartel@gmail.com>
2025-07-25 14:54:37 +0000

    `Removed unused Stack.Screen component from Layout component`

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -7,7 +7,6 @@ const Layout = () => {
 	
 	return (
 		<Stack>
-			<Stack.Screen name="Welcome" options={{ headerShown: false }} />
             <Stack.Screen name="sign-up" options={{ headerShown: false }} />
             <Stack.Screen name="sign-in" options={{ headerShown: false }} />
             <Stack.Screen name="welcome" options={{ headerShown: false }} />
