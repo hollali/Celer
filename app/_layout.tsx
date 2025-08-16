d5abdb3537faace07c657a6b75d800ece6cf42d7
hollali <dheztinykartel@gmail.com>
2025-08-16 04:12:14 +0000

    `Update ClerkProvider with publishable key and remove unused import`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,51 @@
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import './global.css';
-
-
-
+import "./global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+if (!publishableKey) {
+	throw new Error(
+		"Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env file."
+	);
+}
+
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
-    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
-    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
-    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
-    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
-  });
+	const [loaded] = useFonts({
+		"Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+		"Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+		"Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+		"Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+		"Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+		Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+		"Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+	});
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <Stack>
-          <Stack.Screen name="index"options={{headerShown: false,}}/>
-          <Stack.Screen name="(root)"options={{headerShown: false,}}/>
-          <Stack.Screen name="(auth)"options={{headerShown: false,}}/>
-          <Stack.Screen name="+not-found" />
-        </Stack>
-    </ClerkProvider>
-    
-  );
+	return (
+		<ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+			<Stack>
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+				<Stack.Screen name="(root)" options={{ headerShown: false }} />
+				<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+				<Stack.Screen name="+not-found" />
+			</Stack>
+		</ClerkProvider>
+	);
 }
