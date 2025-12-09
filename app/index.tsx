9aefbf582fbff9b0edf56fdb801147df59caba5d
hollali <dheztinykartel@gmail.com>
2025-12-09 15:15:59 +0000

    `Update import statements and fix formatting`

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
-import { useAuth } from '@clerk/clerk-expo'
-import { Redirect } from 'expo-router'
-import React from 'react'
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
+import React from "react";
 const Page = () => {
-  const { isSignedIn } = useAuth()
+  const { isSignedIn } = useAuth();
 
   if (isSignedIn) {
-    return <Redirect href="/(root)/(tabs)/home" />
+    return <Redirect href="/(root)/(tabs)/home" />;
   }
-  return <Redirect href="/(auth)/welcome" />
-}
+  return <Redirect href="/(auth)/welcome" />;
+};
 
 export default Page;
